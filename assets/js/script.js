66b74bcfe93054227c61e6f238be1040b3cd9d44
Lightfooted <vesselofbalance@hotmail.com>
2021-07-30 15:19:03 -0400

    start obtaining weather func

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+const api = {
+    key: '12b15af04b190776a75f0b8edb5f30b8',
+    site: 'https://api.openweathermap.org/data/2.5/weather?',
+  };
+  
+  const Input = document.getElementById('input');
+  
+  Input.addEventListener('keypress', (event) => {
+    if (event) {
+      getWeather(Input.value);
+  
+      document.querySelector('.weather-show').style.display = 'block';
+    }
+  });
+  
+  function getWeather(city) {
+    fetch(`${api.site}q=${city}&appid=${api.key}&units=metric`)
+      .then((data) => {
+        return data.json();
+      })
+  }
