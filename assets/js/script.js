2e591830e39043ebb14fa2a0d6a263dad15e7cd8
Lightfooted <vesselofbalance@hotmail.com>
2021-07-31 13:26:25 -0400

    uv index setup and working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,61 @@
 const api = {
-    key: '12b15af04b190776a75f0b8edb5f30b8',
-    site: 'https://api.openweathermap.org/data/2.5/weather?',
-  };
+  key: '12b15af04b190776a75f0b8edb5f30b8',
+  site: 'https://api.openweathermap.org/data/2.5/weather?',
+  site2: 'https://api.openweathermap.org/data/2.5/uvi'
+};
   
-  const Input = document.getElementById('input');
-  
-  Input.addEventListener('keypress', (event) => {
-    if (event) {
-      getWeather(Input.value);
-  
-      document.querySelector('.weather-show').style.display = 'block';
-    }
-  });
-  
-  function getWeather(city) {
-    fetch(`${api.site}q=${city}&appid=${api.key}&units=metric`)
-      .then((data) => {
-        return data.json();
-      })
-      .then(showWeather);
+const Input = document.getElementById('input');
+
+Input.addEventListener('keypress', (event) => {
+  if (event) {
+    getWeather(Input.value);
+
+    document.querySelector('.weather-show').style.display = 'block';
   }
+});
 
-  function showWeather(data) {
+function getWeather(city) {
+  fetch(`${api.site}q=${city}&appid=${api.key}&units=metric`)
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      return getUvi(data)
+    });
+};
 
-    let city = document.getElementById('city');
-    city.innerHTML = `${data.name}, ${data.sys.country}`;
+function getUvi(data) {
+  const lat = data.coord.lat;
+  const lon = data.coord.lon;
 
-    let temperature = document.getElementById('temp');
-    temperature.innerHTML = `Temp: ${Math.round(data.main.temp)}°C`;
+  fetch(`${api.site2}?appid=${api.key}&lat=${lat}&lon=${lon}`)
+    .then(response => response.json())
+    .then(uviData => {
+      const uvi = uviData.value;
+      return showWeather(data, uvi);
+    });
+};
 
-    let minMax = document.getElementById('min-max');
-    minMax.innerHTML = `${Math.round(data.main.temp_min)}°C (Min) - ${Math.round(data.main.temp_max)}°C (Max)`;
+function showWeather(data, uvi) {
 
-    let windSpeed = document.getElementById('wind');
-    windSpeed.innerHTML = `Wind: ${(data.wind.speed)} MPH`;
+  let city = document.getElementById('city');
+  city.innerHTML = `${data.name}, ${data.sys.country}`;
 
-    let humid = document.getElementById('humidity');
-    humid.innerHTML = `Humidity: ${(data.main.humidity)}%`;
+  let temperature = document.getElementById('temp');
+  temperature.innerHTML = `Temp: ${Math.round(data.main.temp)}°C`;
 
-    let weather = document.getElementById('weather');
-    weather.innerHTML = `Sky: ${data.weather[0].description}`; //Need to figure out UV index
-  }
+  let minMax = document.getElementById('min-max');
+  minMax.innerHTML = `${Math.round(data.main.temp_min)}°C (Min) - ${Math.round(data.main.temp_max)}°C (Max)`;
+
+  let windSpeed = document.getElementById('wind');
+  windSpeed.innerHTML = `Wind: ${(data.wind.speed)} MPH`;
+
+  let humid = document.getElementById('humidity');
+  humid.innerHTML = `Humidity: ${(data.main.humidity)}%`;
+
+  let weather = document.getElementById('weather');
+  weather.innerHTML = `Sky: ${data.weather[0].description}`;
+
+  let uv = document.getElementById('uv');
+  uv.innerHTML = `UV-Index: ${uvi}`;
+}
